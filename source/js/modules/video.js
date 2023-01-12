dfa95270ad0272e1fd3216fd935cfd56ca3d2487
Yana <ten777yana@mail.ru>
2023-01-13 00:33:37 +0300

    complete gym section

---
+++ source/js/modules/video.js
@@ -0,0 +1,25 @@
+// const video = document.querySelector('.gym-section__video');
+// const link = video.querySelector('.gym-section__video-link');
+// const button = video.querySelector('.gym-section__video-button');
+
+// const createIframe = () => {
+//   const iframe = document.createElement('iframe');
+
+//   iframe.setAttribute('allowfullscreen', '');
+//   iframe.setAttribute('allow', 'autoplay');
+//   iframe.style.width = '100%';
+//   iframe.style.height = '100%';
+//   iframe.style.border = 'none';
+//   iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?rel=0&showinfo=0&autoplay=1');
+
+//   return iframe;
+// };
+
+// link.removeAttribute('href');
+
+// button.addEventListener('click', () => {
+//   const iframe = createIframe();
+//   link.remove();
+//   button.remove();
+//   video.appendChild(iframe);
+// });
